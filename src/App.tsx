e55cb5cd54018eb2ac1a40dd4a36901323521ff7
Kayleigh Bell <kayleightuppen@gmail.com>
2023-10-24 15:55:17 +0100

    adding to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import Navbar from './NavBar/NavBar';
 import Example from './SideBar/SideBar';
 import Cards from './Cards/Cards';
+import ProductList from './Cards/CardList';
+import productData, { Product } from './Cards/ProductData';
+
 
 const App: React.FC = () => {
   return (
     <div>
       <Navbar />
       <Example />
-      <Cards />
+      <div className="container mt-4">
+      <ProductList products={productData as Product[]} />
+    </div>
+
     </div>
   );
 }
